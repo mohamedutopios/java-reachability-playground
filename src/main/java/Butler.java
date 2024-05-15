c098376d2d1a23cfcc0396b056f70415db1286ed
mohamed <mohamed@utopios.net>
2024-05-15 07:24:38 +0200

    add correction operateur

--- src/main/java/Butler.java
+++ src/main/java/Butler.java
@@ -5,11 +5,10 @@ public class Butler {
 
     public void welcome() {
 
-        // https://app.snyk.io/vuln/SNYK-JAVA-COMMONSCOLLECTIONS-472711
-        // Should appear as Potentially Reachable
-        ArrayList<String> list1 = new ArrayList<String>();
+
+        ArrayList<String> list1 = new ArrayList<>();
         list1.add("Hello");
-        ArrayList<String> list2 = new ArrayList<String>();
+        ArrayList<String> list2 = new ArrayList<>();
         list2.add("World");
 
         System.out.println(ListUtils.union(list1, list2));
